6614de9b8408af8791506bf4b10888badbea9038
Ben <benbatres1212@gmail.com>
2024-11-16 13:47:41 -0800

    day 1 complete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,15 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        'gradient-to-b':
+          'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 68%,#141414 100%);',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+    require('tailwindcss-textshadow'),
+    require('tailwind-scrollbar'),
+],
 } satisfies Config;
